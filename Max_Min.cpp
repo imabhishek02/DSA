138979db023954c513736734ae7b3a98aff43e21
imabhishek02 <abhishekrohit369@gmail.com>
2021-10-11 12:13:25 +0530

    initial commit

---
+++ Max_Min.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+
+  int arr[n];
+   
+  for (int i=0;i<n;i++){
+      cin>>arr[i];
+}
+int max=INT_MIN;
+for(int i=0;i<n;i++){
+    if(max < arr[i])
+    max=arr[i];
+}
+int min=INT_MAX;
+for(int i=0;i<n;i++){
+    if (min>arr[i]){
+        min=arr[i];
+    }
+}
+cout<<"the maximun is"<< max<<endl;
+cout<<"the minimun is"<<min;
+
+
+
+   
+    return 0;
+}
