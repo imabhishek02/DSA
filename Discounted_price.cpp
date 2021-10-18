2c21baeb4c61c056bee695abdcf3b2786c307430
imabhishek02 <abhishekrohit369@gmail.com>
2021-10-18 23:35:00 +0530

    initial commit

---
+++ Discounted_price.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double items[] = {500, 12.4, 94, 45, 3, 81, 1000.9, 85, 90, 1, 35};
+    double pf;
+    cin>>pf;
+
+    for( int i=0;i<11;i++){
+        double dpf=0;
+        dpf = items[i]- (items[i]*(pf/100));
+        cout<<dpf<<" ";
+        
+    }
+    
+    
+    return 0;
+}
